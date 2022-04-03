61b47f72da7ced6f1eef90dfda5609be7088cd87
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-04-03 18:11:45 +0800

    ah yes garamond is da best

--- 2022-year2/math/problem-of-the-day/.auctex-auto/main.el
+++ 2022-year2/math/problem-of-the-day/.auctex-auto/main.el
@@ -4,13 +4,14 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("exam" "12pt" "answers")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("geometry" "margin=1in" "a4paper")))
+                     '(("geometry" "margin=1in" "a4paper") ("mathdesign" "urw-garamond") ("fontenc" "T1")))
    (TeX-run-style-hooks
     "latex2e"
     "exam"
     "exam12"
     "geometry"
-    "mathpazo"
+    "mathdesign"
+    "fontenc"
     "amsmath"
     "amssymb"
     "amsthm"
