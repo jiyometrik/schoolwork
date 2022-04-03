6b65bc9bd48827b69a80a013eefd9e150ec35030
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-04-03 15:50:39 +0800

    aegopajgep

--- 2022-year2/math/problem-of-the-day/.auctex-auto/main.el
+++ 2022-year2/math/problem-of-the-day/.auctex-auto/main.el
@@ -4,16 +4,13 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("exam" "12pt" "answers")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("geometry" "margin=1in" "a4paper") ("newtxmath" "cmintegrals" "cmbraces")))
+                     '(("geometry" "margin=1in" "a4paper")))
    (TeX-run-style-hooks
     "latex2e"
     "exam"
     "exam12"
     "geometry"
-    "newtxmath"
-    "ebgaramond-maths"
     "amsmath"
-    "amsthm"
     "amssymb"
     "siunitx"
     "tikz"
