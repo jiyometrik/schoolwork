6eb9e811b7c1c98e63bbdc8d783ecfb5baa706a8
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-04-05 20:45:00 +0800

    potb 1/4 and 4/4

--- 2022-year2/math/problem-of-the-day/.auctex-auto/main.el
+++ 2022-year2/math/problem-of-the-day/.auctex-auto/main.el
@@ -4,20 +4,21 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("exam" "12pt" "answers")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("geometry" "margin=1in" "a4paper") ("mathdesign" "urw-garamond") ("fontenc" "T1")))
+                     '(("geometry" "margin=1in" "a4paper")))
    (TeX-run-style-hooks
     "latex2e"
     "exam"
     "exam12"
     "geometry"
-    "mathdesign"
-    "fontenc"
+    "mathpazo"
     "amsmath"
     "amssymb"
     "amsthm"
     "siunitx"
     "tikz"
     "setspace"
-    "cancel"))
+    "cancel"
+    "cases"
+    "showlabels"))
  :latex)
 
